8723642475a8ecf3064a2d96bc4bd566b98040cc
Mohaned-Adel <mohaned.adel1996@gmail.com>
2023-02-28 11:58:35 +0200

    finish aside panel with card data json file

--- src/app/recently-visit/recently-visit.component.ts
+++ src/app/recently-visit/recently-visit.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import dataCard from './data.json';
 
+interface Card {
+  title: String;
+  imageSrc: String;
+  price: Number;
+  newPrice: Number;
+  offer: Boolean;
+  new: Boolean;
+}
 @Component({
   selector: 'app-recently-visit',
   templateUrl: './recently-visit.component.html',
-  styleUrls: ['./recently-visit.component.css']
+  styleUrls: ['./recently-visit.component.css'],
 })
 export class RecentlyVisitComponent {
-
+  cardData: Card[] = dataCard;
 }
